77301b8c9c478918f4d902c208f0114314a94585
Alan <ajarias4@espe.edu.ec>
2024-12-01 19:51:46 -0500

    error fixed due to version mixing

--- 06-Code/SAMC/src/espe/edu/ec/controller/ManageFileCsv.java
+++ 06-Code/SAMC/src/espe/edu/ec/controller/ManageFileCsv.java
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 package espe.edu.ec.controller;
 
 import espe.edu.ec.model.Cashier;
-import espe.edu.ec.model.Customer;
-import espe.edu.ec.model.Order;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,58 +12,8 @@ public class ManageFileCsv {
     private static final String FILE_NAME = "sales.csv";
 
     public void saveToCsv(Cashier cashier, Map<String, Integer> order, float totalToPay) {
-        
         try (FileWriter writer = new FileWriter(FILE_NAME, true)) {
-            
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String date = dateFormat.format(new Date());
-
-            writer.append(cashier.getCustomer().getName()).append(",");
-            writer.append(String.valueOf(cashier.getCustomer().getId())).append(",");
-            writer.append(cashier.getCustomer().getEmail()).append(",");
-            writer.append(cashier.getCustomer().getPhoneNumber()).append(",");
-
-            
-            StringBuilder dishes = new StringBuilder();
-            StringBuilder quantities = new StringBuilder();
-            for (Map.Entry<String, Integer> entry : order.entrySet()) {
-                dishes.append(entry.getKey()).append(";");
-                quantities.append(entry.getValue()).append(";");
-            }
-
-            
-            writer.append(dishes.toString()).append(",");
-            writer.append(quantities.toString()).append(",");
-            writer.append(date).append(",");
-            writer.append(String.valueOf(totalToPay)).append("\n");
-
-            System.out.println("Datos guardados en el archivo CSV.");
-
-        } catch (IOException e) {
-            System.out.println("Error al guardar en CSV: " + e.getMessage());
-        }
-    }
-}
-=======
-package espe.edu.ec.controller;
-
-import espe.edu.ec.model.Cashier;
-import espe.edu.ec.model.Customer;
-import espe.edu.ec.model.Order;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-
-public class ManageFileCsv {
-    private static final String FILE_NAME = "sales.csv";
-
-    public void saveToCsv(Cashier cashier, Map<String, Integer> order, float totalToPay) {
-        
-        try (FileWriter writer = new FileWriter(FILE_NAME, true)) {
-            
+            // Formatear la fecha actual
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             String date = dateFormat.format(new Date());
 
@@ -76,7 +23,7 @@ public class ManageFileCsv {
             writer.append(cashier.getCustomer().getEmail()).append(",");
             writer.append(cashier.getCustomer().getPhoneNumber()).append(",");
 
-            
+            // Escribir los platos y cantidades
             StringBuilder dishes = new StringBuilder();
             StringBuilder quantities = new StringBuilder();
             for (Map.Entry<String, Integer> entry : order.entrySet()) {
@@ -84,17 +31,14 @@ public class ManageFileCsv {
                 quantities.append(entry.getValue()).append(";");
             }
 
-            
             writer.append(dishes.toString()).append(",");
             writer.append(quantities.toString()).append(",");
             writer.append(date).append(",");
             writer.append(String.valueOf(totalToPay)).append("\n");
 
             System.out.println("Datos guardados en el archivo CSV.");
-
         } catch (IOException e) {
             System.out.println("Error al guardar en CSV: " + e.getMessage());
         }
     }
 }
->>>>>>> bedba1bb70b39b56981fa0fafbc49aea54d01497
--- 06-Code/SAMC/src/espe/edu/ec/model/Customer.java
+++ 06-Code/SAMC/src/espe/edu/ec/model/Customer.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package espe.edu.ec.model;
 
 import java.util.List;
@@ -57,64 +56,3 @@ public class Customer {
         return phoneNumber;
     }
 }
-=======
-package espe.edu.ec.model;
-
-import java.util.List;
-
-public class Customer {
-    private String name;
-    private int id;
-    private String email;
-    private String address;
-    private String phoneNumber;
-    private List<Order> orders;
-
-    public Customer(String name, int id, String email, String address, String phoneNumber) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder ordersDetails = new StringBuilder();
-        if (orders != null) {
-            for (Order order : orders) {
-                ordersDetails.append(order).append("\n");
-            }
-        }
-        return "Customer{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", orders=\n" + ordersDetails +
-                '}';
-    }
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-}
->>>>>>> bedba1bb70b39b56981fa0fafbc49aea54d01497
