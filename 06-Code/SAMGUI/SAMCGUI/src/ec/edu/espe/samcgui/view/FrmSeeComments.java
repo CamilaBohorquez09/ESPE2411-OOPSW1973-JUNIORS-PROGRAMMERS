2619af70b9874102f868ad7a2fa5bbe33a9b1b5e
Robinson Eduardo Bonilla Chapigo <robinsoneduboch@gmail.com>
2025-01-24 00:43:32 -0500

    FrmSeeComments screen added

--- 06-Code/SAMGUI/SAMCGUI/src/ec/edu/espe/samcgui/view/FrmSeeComments.java
+++ 06-Code/SAMGUI/SAMCGUI/src/ec/edu/espe/samcgui/view/FrmSeeComments.java
@@ -157,6 +157,8 @@ public class FrmSeeComments extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
